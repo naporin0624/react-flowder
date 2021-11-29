f973ef2eb5449eaeaacbab32af236d76e4227b54
naporin0624 <naporin0624@gmail.com>
2021-11-29 16:50:11 +0900

    :+1: create directory for react-flow lib

---
+++ packages/react-flow/.eslintrc.js
@@ -0,0 +1,22 @@
+const config = require("../../.eslintrc.js");
+
+module.exports = {
+  ...config,
+  parserOptions: {
+    ...config.parserOptions,
+    jsx: true,
+  },
+  plugins: [...config.plugins, "react-hooks"],
+  extends: [...config.extends, "plugin:react/recommended"],
+  root: false,
+  env: {
+    node: false,
+    jest: true,
+    browser: true,
+  },
+  rules: {
+    ...config.rules,
+    "react/prop-types": "off",
+    "react/display-name": "off",
+  }
+};
---
+++ packages/react-flow/README.md
@@ -0,0 +1,11 @@
+# `react-sync-loader`
+
+> TODO: description
+
+## Usage
+
+```
+const reactSyncLoader = require('react-sync-loader');
+
+// TODO: DEMONSTRATE API
+```
