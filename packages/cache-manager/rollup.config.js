c564c0e0745a4ccd1d00ead3aaed0a9bbb4ae433
naporin0624 <naporin0624@gmail.com>
2021-12-01 22:01:28 +0900

    :+1: react も含める

--- packages/cache-manager/rollup.config.js
+++ packages/cache-manager/rollup.config.js
@@ -1,3 +1,36 @@
-import config from "../../rollup.config";
+import base from "../../rollup.config";
+import typescript from "rollup-plugin-typescript2";
+import dts from "rollup-plugin-dts";
+
+const config = [
+  ...base,
+  {
+    input: "./src/react/index.ts",
+    output: [{ file: "lib/react/index-esm.js", format: "es" }],
+    plugins: [
+      typescript({
+        tsconfigOverride: {
+          exclude: ["**/__tests__", "**/*.test.ts"],
+        },
+      }),
+    ],
+  },
+  {
+    input: "./src/react/index.ts",
+    output: [{ file: "lib/react/index-cjs.js", format: "cjs", exports: "named" }],
+    plugins: [
+      typescript({
+        tsconfigOverride: {
+          exclude: ["**/__tests__", "**/*.test.ts"],
+        },
+      }),
+    ],
+  },
+  {
+    input: "./src/react/index.ts",
+    output: [{ file: "lib/react/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
 
 export default config;
