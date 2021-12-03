7bc2f8b150413395331a7abe39798eae5f17448a
naporin0624 <naporin0624@gmail.com>
2021-12-04 01:20:52 +0900

    :+1: cacheStore を変更できるように

--- packages/react-loader/__tests__/index.test.tsx
+++ packages/react-loader/__tests__/index.test.tsx
@@ -1,10 +1,6 @@
 import React, { FC, useState } from "react";
-import { act, renderHook } from "@testing-library/react-hooks";
-import { Cache, useLoader, useCacheKey, useCache } from "../src";
-
-const Wrapper: FC = ({ children }) => {
-  return <Cache>{children}</Cache>;
-};
+import { act, renderHook, cleanup } from "@testing-library/react-hooks";
+import { Cache, useLoader, useCacheKey, useCache, createCacheStore } from "../src";
 
 const timeout = (ms = 10, error?: boolean): Promise<number> => {
   return new Promise((resolve, reject) =>
@@ -14,53 +10,88 @@ const timeout = (ms = 10, error?: boolean): Promise<number> => {
   );
 };
 
-describe("test react-loader", () => {
-  describe("useCacheKey", () => {
-    test("simple usage", async () => {
+describe("react-loader test", () => {
+  afterEach(cleanup);
+  describe("inject provider test", () => {
+    const cacheStore = createCacheStore();
+    const config = {
+      provider: cacheStore,
+    };
+    const wrapper: FC = ({ children }) => {
+      return <Cache config={config}>{children}</Cache>;
+    };
+
+    beforeEach(() => {
+      cacheStore.clear();
+    });
+
+    test("provider equal hook", async () => {
+      const key = "timeout";
+      const { result, waitForNextUpdate } = renderHook(() => useLoader(key, timeout(10)), { wrapper });
+      expect(result.current).toEqual(undefined);
+      await waitForNextUpdate();
+
+      expect(result.current).toEqual(10);
+      expect(result.current === cacheStore.get(key)?.payload).toBe(true);
+    });
+
+    test("change cacheStore after rerender test", async () => {
+      const key = "timeout";
+      const { result, waitForNextUpdate, rerender } = renderHook(() => useLoader(key, timeout(10)), { wrapper });
+      expect(result.current).toEqual(undefined);
+      await waitForNextUpdate();
+      expect(result.current).toEqual(10);
+
+      cacheStore.set(key, { type: "success", payload: 100 });
+      expect(result.current).toEqual(10);
+      rerender();
+      expect(result.current).toEqual(100);
+    });
+  });
+
+  describe("hook test", () => {
+    const wrapper: FC = ({ children }) => {
+      return <Cache>{children}</Cache>;
+    };
+    test("useCacheKey test", async () => {
       const { result, waitForNextUpdate } = renderHook(
         () => {
           useLoader("timeout", timeout(20, false));
           return useCacheKey();
         },
-        {
-          wrapper: Wrapper,
-        }
+        { wrapper }
       );
       await waitForNextUpdate();
 
       expect(result.current).toEqual(expect.arrayContaining(["timeout"]));
     });
-  });
-  describe("useCache", () => {
-    test("simple usage", async () => {
+    test("useCache test", async () => {
       const { result, waitForNextUpdate } = renderHook(
         () => {
           useLoader("timeout", timeout(20, false));
           return useCache();
         },
-        { wrapper: Wrapper }
+        { wrapper }
       );
       await waitForNextUpdate();
 
       expect(result.current.get("timeout")).toEqual({ payload: 20, type: "success" });
     });
-  });
-  describe("userLoader", () => {
-    test("simple usage", async () => {
-      const { result, waitForNextUpdate } = renderHook(() => useLoader("timeout", timeout(10)), { wrapper: Wrapper });
+    test("useLoader test", async () => {
+      const { result, waitForNextUpdate } = renderHook(() => useLoader("timeout", timeout(10)), { wrapper: wrapper });
       await waitForNextUpdate();
 
       expect(result.current).toBe(10);
     });
 
-    test("useLoader & useState", async () => {
+    test("useLoader & useState test", async () => {
       const { result, waitForNextUpdate } = renderHook(
         () => {
           const [count, setCount] = useState(10);
           const data = useLoader(`timeout__${count}`, timeout(count));
           return [data, setCount] as const;
         },
-        { wrapper: Wrapper }
+        { wrapper: wrapper }
       );
       await waitForNextUpdate();
       expect(result.current[0]).toBe(10);
@@ -69,8 +100,8 @@ describe("test react-loader", () => {
       await waitForNextUpdate();
       expect(result.current[0]).toBe(20);
     });
-    test("reject", async () => {
-      const { result, waitForNextUpdate } = renderHook(() => useLoader("reject", timeout(0, true)), { wrapper: Wrapper });
+    test("useLoader error occurred test", async () => {
+      const { result, waitForNextUpdate } = renderHook(() => useLoader("reject", timeout(0, true)), { wrapper: wrapper });
       await waitForNextUpdate();
       return expect(result.error).toEqual(new Error("error"));
     });
--- packages/react-loader/src/Provider.tsx
+++ packages/react-loader/src/Provider.tsx
@@ -1,7 +1,14 @@
 import React, { useMemo, FC } from "react";
-import { CacheContext, createCacheStore } from "./cache";
+import { CacheContext, createCacheStore, CacheStore } from "./cache";
 
-export const Provider: FC = ({ children }) => {
-  const cache = useMemo(() => createCacheStore(), []);
+type Props = {
+  config?: {
+    provider?: CacheStore;
+  };
+};
+
+export const Provider: FC<Props> = ({ children, config }) => {
+  const cache = useMemo(() => config?.provider ?? createCacheStore(), [config?.provider]);
+
   return <CacheContext.Provider value={cache}>{children}</CacheContext.Provider>;
 };
