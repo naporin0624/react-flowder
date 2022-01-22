c41bceaad115911222300e820fdf8b6907d8f41c
naporin0624 <naporin0624@gmail.com>
2022-01-22 09:06:07 +0900

    :+1: external store を共有する

--- packages/react-flowder/src/Provider.tsx
+++ packages/react-flowder/src/Provider.tsx
@@ -1,11 +1,28 @@
 import React, { FC } from "react";
 import { Cache } from "@naporin0624/react-loader";
 import { Provider as Flow } from "@naporin0624/react-flow";
+import { createStore } from "@naporin0624/simple-store";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Status<T = any> =
+  | {
+      type: "success";
+      payload: T;
+    }
+  | {
+      type: "error";
+      payload: Error;
+    };
+
+const store = createStore<string, Status>();
+const config = {
+  provider: store,
+};
 
 export const Provider: FC = ({ children }) => {
   return (
-    <Flow>
-      <Cache>{children}</Cache>
+    <Flow config={config}>
+      <Cache config={config}>{children}</Cache>
     </Flow>
   );
 };
