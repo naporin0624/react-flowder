3a7622916b17af671bf9238c2d47b7d8c0de0055
naporin0624 <naporin0624@gmail.com>
2022-04-09 03:04:05 +0900

    :+1: update apollonius

--- packages/apollonius/src/index.ts
+++ packages/apollonius/src/index.ts
@@ -1,23 +1,40 @@
-import { useMemo } from "react";
+import { datasource, useReadData, Datasource } from "@naporin0624/react-flowder";
+import { DocumentNode, OperationVariables, QueryOptions, useApolloClient } from "@apollo/client";
+import { Observable } from "rxjs";
+import type { TypedDocumentNode } from "@apollo/client";
 
-import { NetworkStatus, OperationVariables, QueryOptions, useApolloClient } from "@apollo/client";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const cacheMap = new WeakMap<DocumentNode, Datasource<[Omit<QueryOptions<any, any>, "query"> | undefined], any>>();
 
-export function useQuery<T = never, V = OperationVariables>(query: QueryOptions<V, T>["query"], options?: Omit<QueryOptions<V, T>, "query">): T {
+const useDatasource = <T = never, V = OperationVariables>(query: TypedDocumentNode<T, V>): Datasource<[Omit<QueryOptions<V, T>, "query"> | undefined], T> => {
   const client = useApolloClient();
-  const queryOptions: QueryOptions<V, T> = useMemo(() => ({ query, ...(options ? options : {}) }), [options, query]);
 
-  const subscription = client.watchQuery(queryOptions);
-  const result = subscription.getCurrentResult();
+  const cache = cacheMap.get(query);
+  if (cache) return cache;
 
-  switch (result.networkStatus) {
-    case NetworkStatus.loading: {
-      throw subscription.result();
-    }
-    case NetworkStatus.error: {
-      throw subscription.getLastError();
-    }
-    default: {
-      return subscription.getCurrentResult().data;
-    }
-  }
+  const observable = (options?: Omit<QueryOptions<V, T>, "query">) => {
+    const subscription = client.watchQuery({ query, ...options });
+    return new Observable<T>((subscriber) => {
+      subscription.subscribe(
+        (next) =>
+          next.data
+            ? subscriber.next(next.data)
+            : {
+                /** noop */
+              },
+        (err) => subscriber.error(err),
+        () => subscriber.complete()
+      );
+    });
+  };
+
+  const resource = datasource(observable);
+  cacheMap.set(query, resource);
+  return resource;
+};
+
+export function useQuery<T = never, V extends OperationVariables = OperationVariables>(query: QueryOptions<V, T>["query"], options?: Omit<QueryOptions<V, T>, "query">): T {
+  const datasource = useDatasource(query);
+
+  return useReadData(datasource(options));
 }
