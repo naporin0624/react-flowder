187974e2a5513865edd6e5f272aa3f7a357a555d
naporin0624 <naporin0624@gmail.com>
2022-04-11 00:18:28 +0900

    :shower: delete unused package

--- packages/cache-manager/src/react/context.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext } from "react";
-
-import type { CacheManager } from "../core";
-
-export const Context = createContext<CacheManager<any> | null>(null);
--- packages/cache-manager/src/react/index.ts
+++
@@ -1,5 +0,0 @@
-export { Provider } from "./Provider";
-export { Context as CacheManageContext } from "./context";
-export { useCache } from "./hooks";
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface DefaultCache {}
